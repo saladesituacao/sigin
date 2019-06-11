e9b3b2a4b0bb252acbb86c2abfe1c1c03ad948df
José Roberto Corrêa de Vasconcelos <jose.roberto@dinamonetworks.com>
2019-06-10 23:56:48 -0300

    #Fix: build

---
+++ client/src/environments/environment.docker.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  url: "http://api.saude.df.gov.br",
+  API_URL: "/api",
+  aplicacao: 1, // codigo da aplicacao SIGIN
+  urlNewUser: "",
+  urlForgetPassword: ""
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  //url: "http://localhost:8000",
+  url: "http://api.saude.df.gov.br",
+  API_URL: "/api",
+  aplicacao: "MGI", // codigo da aplicacao SIGIN
+  urlNewUser: "",
+  urlForgetPassword: "",
+  urlAuth: "https://api.saude.df.gov.br/auth"
+};
