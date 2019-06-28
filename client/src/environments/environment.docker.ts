d57489db54e67e1619e19caf42e631ecdd98f213
José Roberto Corrêa de Vasconcelos <jose.roberto@dinamonetworks.com>
2019-06-28 09:10:43 -0300

    #new: Criacao de parametrizacao para dockerizar o cliente

--- client/src/environments/environment.docker.ts
+++ client/src/environments/environment.docker.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  url: "http://api.saude.df.gov.br",
-  API_URL: "/api",
+  url: 'URL_REST',
+  API_URL: "CONTEXT_REST",
   aplicacao: 1, // codigo da aplicacao SIGIN
-  urlNewUser: "",
-  urlForgetPassword: ""
+  urlNewUser: "http://",
+  urlForgetPassword: "http://",
+  urlAuth: "URL_AUTH"
 };
